62f4478e71286de5cdbaeb9737c483f9e695f70d
Do Trung Tu <tudo1611@gmail.com>
2023-03-20 09:44:15 +0700

    update

--- js/main.js
+++ js/main.js
@@ -9,6 +9,11 @@ var result5 = document.querySelector("#result5");
 var result6 = document.querySelector("#result6");
 var result7 = document.querySelector("#result7");
 var result8 = document.querySelector("#result8");
+var result9 = document.querySelector("#result9");
+var inputNum9 = document.querySelector("#inputNum9");
+var resultInput9 = document.querySelector("#resultInput9");
+var result10 = document.querySelector("#result10");
+
 
 
 document.querySelector("#btnInput").addEventListener("click", function () {
@@ -93,19 +98,19 @@ function swap(array, position1, position2) {
   array[position2] = temp;
   return array;
   //cách 2
-//   [array[position1], array[position2]] = [array[position2]], [array[position1]]
-//   return array;
+  //   [array[position1], array[position2]] = [array[position2]], [array[position1]]
+  //   return array;
 }
 
 document.querySelector("#btnDoiCho").addEventListener("click", function () {
   var viTri1 = document.querySelector("#viTri1").value * 1;
   var viTri2 = document.querySelector("#viTri2").value * 1;
-  result6.innerHTML ='Mảng sau khi đổi: ' + swap(arrayNum, viTri1, viTri2);
+  result6.innerHTML = "Mảng sau khi đổi: " + swap(arrayNum, viTri1, viTri2);
 });
 
 //7- sắp xếp tăng dần
-function compareNumbers(a,b) {
-    return a - b;
+function compareNumbers(a, b) {
+  return a - b;
 }
 document
   .querySelector("#btnSapXepTangDan")
@@ -114,24 +119,88 @@ document
       "Mảng sau khi sắp xếp: " + arrayNum.sort(compareNumbers);
   });
 
-  //8- tìm số nguyên tố đầu tiên
+//8- tìm số nguyên tố đầu tiên
 function kiemTraSoNguyenTo(number) {
   var checkSNT = true;
-  for (var i = 2; i <= Math.sqrt(number); i++) {
-    if (number % i === 0) {
-      checkSNT = false;
-      break;
+  if (number < 2) {
+    checkSNT = false;
+  } else {
+    for (var i = 2; i <= Math.sqrt(number); i++) {
+      if (number % i === 0) {
+        checkSNT = false;
+        break;
+      }
     }
   }
   return checkSNT;
 }
-  document
-    .querySelector("#btnTimSoNguyenToDauTien")
-    .addEventListener("click", function () {
-      for (var i = 0; i < arrayNum.length; i++) {
-        if (kiemTraSoNguyenTo(arrayNum[i]) === true);
-        return arrayNum[i];
+document
+  .querySelector("#btnTimSoNguyenToDauTien")
+  .addEventListener("click", function () {
+    let soNguyenToDauTien;
+    for (var i = 0; i < arrayNum.length; i++) {
+      if (kiemTraSoNguyenTo(arrayNum[i])) {
+        soNguyenToDauTien = arrayNum[i];
+        break;
       }
-      result8.innerHTML = "Số nguyên tố đầu tiên: " + arrayNum[i];
-    });
+    }
+    result8.innerHTML = "Số nguyên tố đầu tiên: " + soNguyenToDauTien;
+  });
 
+//9 - Đếm số nguyên
+// hàm check số nguyên
+function check_real_leteger_number(n) {
+  let flag = 1;
+  if (Math.ceil(n) != Math.floor(n)) flag = 0;
+  return flag;
+}
+
+document.querySelector("#btnInput9").addEventListener("click", function () {
+  arrayNum.push(inputNum9.value.trim());
+  resultInput9.innerHTML = arrayNum;
+});
+
+document
+  .querySelector("#btnDemSoNguyen")
+  .addEventListener("click", function () {
+    var count = 0;
+    for (var i = 0; i < arrayNum.length; i++) {
+      if (check_real_leteger_number(arrayNum[i])) {
+        count++;
+      }
+    }
+    result9.innerHTML = "Có: " + count + " số nguyên";
+  });
+
+//10 - so sánh số âm và số dương
+
+// function check_negative_positive_number(n) {
+//   //flag = -1 => số âm
+//   //flag =  0 =>  số 0
+//   //flag =  1 => số dương
+
+//   let flag = 0;
+//   if (n > 0) flag = 1;
+//   else if (n < 0) flag = -1;
+//   return flag;
+// }
+
+document.querySelector("#btnSoSanh").addEventListener('click', function () {
+  var soAm = [];
+  var soDuong = [];
+  for ( var i = 0; i < arrayNum.length; i++) {
+    if (arrayNum[i] > 0) {
+      soDuong.push(arrayNum[i]);
+    }
+    if (arrayNum[i] < 0) {
+      soAm.push(arrayNum[i]);
+    }
+  }
+  if ( soAm.length == soDuong.length) {
+    result10.innerHTML = 'số âm = số dương';
+  } else if ( soAm.length > soDuong.length) {
+    result10.innerHTML = 'số âm > số dương';
+  } else {
+    result10.innerHTML = 'số âm < số dương';
+  }
+});
